bf56c299f7f83a84508ac7415c62ba0dd63d9fe4
maituankiet01042k7-ctrl <maituankiet01042k7@gmail.com>
2026-01-29 21:59:56 +0700

    Create ExFive.py

---
+++ Chapter01/ExFive.py
@@ -0,0 +1,17 @@
+# Bai 5
+name = "Mai Tuan Kiet"
+age = 19
+average_score = 8.5
+
+print(type(name))
+print(type(age))
+print(type(average_score))
+
+age_next_year = age + 1
+doubled_score = average_score * 2
+
+print("Name:", name)
+print("Age:", age)
+print("Average Score:", average_score)
+print("Age Next Year:", age_next_year)
+print("Doubled Score:", doubled_score)
