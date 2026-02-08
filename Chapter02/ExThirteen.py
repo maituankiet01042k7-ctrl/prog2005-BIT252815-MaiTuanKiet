4099f5bc5c5b3f5073849e05c6132d517093187b
maituankiet01042k7-ctrl <maituankiet01042k7@gmail.com>
2026-02-09 00:30:33 +0700

    Create ExThirteen.py

---
+++ Chapter02/ExThirteen.py
@@ -0,0 +1,8 @@
+# Bai 13
+while True:
+    password = input("Nhập mật khẩu: ")
+    if password == "python123":
+        print("Mật khẩu đúng. Đăng nhập thành công!")
+        break
+    else:
+        print("Mật khẩu sai. Vui lòng nhập lại.")
