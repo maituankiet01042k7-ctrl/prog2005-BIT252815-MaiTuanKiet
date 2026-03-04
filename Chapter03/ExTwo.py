d98113536cd71d8ba68804211d25f0c11672b290
maituankiet01042k7-ctrl <maituankiet01042k7@gmail.com>
2026-03-04 16:30:26 +0700

    Create ExTwo.py

---
+++ Chapter03/ExTwo.py
@@ -0,0 +1,9 @@
+# Bai 2
+friends = ["An", "Bình", "Châu"]
+print("Danh sách ban đầu:", friends)
+
+friends.append("Dũng")
+print("Danh sách sau khi thêm (append):", friends)
+
+friends.pop(1)
+print("Danh sách sau khi xóa vị trí thứ hai (pop):", friends)
