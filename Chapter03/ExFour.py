a46fa97a59eda0c386efb9e05ee0071d6ce5b776
maituankiet01042k7-ctrl <maituankiet01042k7@gmail.com>
2026-03-04 16:42:39 +0700

    Create ExFour.py

---
+++ Chapter03/ExFour.py
@@ -0,0 +1,12 @@
+# Bai 4:
+numbers = [7, 2, 5, 9, 5, 1]
+print("Danh sách ban đầu:", numbers)
+
+numbers.sort()
+print("Danh sách sau khi sắp xếp (sort):", numbers)
+
+numbers.reverse()
+print("Danh sách sau khi đảo ngược (reverse):", numbers)
+
+so_lan_xuat_hien = numbers.count(5)
+print(f"Số lần xuất hiện của số 5 là: {so_lan_xuat_hien}")
