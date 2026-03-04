0a89f34c0ae52db39970c47290f8655fc5137205
maituankiet01042k7-ctrl <maituankiet01042k7@gmail.com>
2026-03-04 16:34:37 +0700

    Delete Chapter3 directory

--- Chapter3/ExOne.py
+++
@@ -1,8 +0,0 @@
-#BAI 1:
-subjects = ["Toán cao cấp", "Lập trình Python", "Tiếng Anh", "Triết học"]
-
-print(f"Số lượng môn học đang học là: {len(subjects)}")
-
-print("Danh sách các môn học:")
-for subject in subjects:
-    print(subject)
