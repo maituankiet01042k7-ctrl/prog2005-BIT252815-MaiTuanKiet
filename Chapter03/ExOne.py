0dc534c166122f426d9cd4bbafa158ca38a43757
maituankiet01042k7-ctrl <maituankiet01042k7@gmail.com>
2026-03-04 16:37:28 +0700

    Create ExOne.py

---
+++ Chapter03/ExOne.py
@@ -0,0 +1,8 @@
+# Bai 1:
+subjects = ["Toán cao cấp", "Lập trình Python", "Tiếng Anh", "Triết học"]
+
+print(f"Số lượng môn học đang học là: {len(subjects)}")
+
+print("Danh sách các môn học:")
+for subject in subjects:
+    print(subject)
