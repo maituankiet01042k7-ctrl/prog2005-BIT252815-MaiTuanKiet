86d8f47118d04b6c1dad5a6dc6fdeadc888547c4
maituankiet01042k7-ctrl <maituankiet01042k7@gmail.com>
2026-03-13 14:33:33 +0700

    Create ExFive.py

---
+++ Chương 4/ExFive.py
@@ -0,0 +1,18 @@
+# Bai 5:
+class Product:
+    def __init__(self, price):
+        self.price = price 
+        
+    @property
+    def price(self):
+        return self._price
+        
+    @price.setter
+    def price(self, value):
+        if value > 0:
+            self._price = value
+        else:
+            raise ValueError("Giá phải lớn hơn 0")
+            
+    def __str__(self):
+        return f"Price: {self._price}"
