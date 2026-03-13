264b14b54d1c7e635b28c2ca3b202c726d311fea
maituankiet01042k7-ctrl <maituankiet01042k7@gmail.com>
2026-03-13 14:32:28 +0700

    Create ExFour.py

---
+++ Chương 4/ExFour.py
@@ -0,0 +1,10 @@
+class Hoa:
+    def __init__(self, ten, mau):
+        self.ten = ten
+        self.mau = mau
+        
+    def __str__(self):
+        return f"Hoa: {self.ten}, Màu: {self.mau}"
+
+hoa_hong = Hoa("Hoa Hồng", "Đỏ")
+print(hoa_hong)
