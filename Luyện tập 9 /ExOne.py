c3fa906077cbeafc7e554e345c529b8755e92525
maituankiet01042k7-ctrl <maituankiet01042k7@gmail.com>
2026-03-18 22:03:25 +0700

    Update ExOne.py

--- Luyện tập 9 /ExOne.py
+++ Luyện tập 9 /ExOne.py
@@ -1,3 +1,6 @@
-# Bai 1:
+print("--- CHẠY BÀI 1 ---")
 weight = float(input("Nhập cân nặng (kg): "))
 height = float(input("Nhập chiều cao (m): "))
+bmi = weight / (height * height)
+print(f"BMI = {bmi:.2f}")
+print("-" * 30)
