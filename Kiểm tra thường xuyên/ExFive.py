37eb8429be8e0f467fc788c913a126c75ffad46b
maituankiet01042k7-ctrl <maituankiet01042k7@gmail.com>
2026-04-03 17:30:32 +0700

    Create ExFive.py

---
+++ Kiểm tra thường xuyên/ExFive.py
@@ -0,0 +1,6 @@
+# Bài 5
+with open("books.txt", "w", encoding="utf-8") as file:
+    file.write("Book 1;30000\n")
+    file.write("Book 2;50000\n")
+    file.write("Book 3;100000\n")
+    file.write("Tong;900000\n"
