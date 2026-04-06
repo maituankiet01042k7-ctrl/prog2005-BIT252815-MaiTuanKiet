14f7126399005813fd9f26222d4cb9105342e6c4
maituankiet01042k7-ctrl <maituankiet01042k7@gmail.com>
2026-04-06 17:08:43 +0700

    Create ExSix.py

---
+++ Luyện tập 15/ExSix.py
@@ -0,0 +1,9 @@
+# BÀI 6
+def bai_6():
+    colors = ["Đỏ", "Xanh lá", "Xanh dương", "Vàng", "Tím"]
+    print(f"Danh sách ban đầu: {colors}")
+    colors.remove("Vàng")
+    print(f"Danh sách sau khi xóa 'Vàng': {colors}")
+
+if __name__ == "__main__":
+    bai_6()
