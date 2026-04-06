d6b69ecb62141d9a7fff19cfd71290d620233203
maituankiet01042k7-ctrl <maituankiet01042k7@gmail.com>
2026-04-06 17:17:54 +0700

    Create ExTen.py

---
+++ Luyện tập 15/ExTen.py
@@ -0,0 +1,9 @@
+# BÀI 10
+def bai_10():
+    a = input("Nhập số a: ")
+    b = input("Nhập số b: ")
+    chuoi_format = f"{a}---{b}"
+    print(chuoi_format)
+
+if __name__ == "__main__":
+    bai_10()
